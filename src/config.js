68f178c8511af651ce3848e9b739708c13534ede
Alexander <mail@fomalhaut.su>
2022-09-08 00:03:22 +0300

    hashstorage_root from VUE_APP_HASHSTORAGE_ROOT

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  hashstorage_root: "https://hashstorage2.fomalhaut.su",
+  hashstorage_root: process.env.VUE_APP_HASHSTORAGE_ROOT,
   app_id: "b33030d6-94fe-4577-9190-9960b1e0f94a",
 }
